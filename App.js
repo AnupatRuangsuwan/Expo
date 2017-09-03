cfd4cdda05594cd34b48a144984eb119e6fcf4cf
Anupat Ruangsuwan <anupat.ruangsuwan@gmail.com>
2017-09-03 16:53:35 +0700

    Before Detach

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props){
+      super(props)
+      this.state = {
+        index: 0
+      }
+  }
+
   render() {
     return (
       <View style={styles.container}>
+
         <Text>Open up App.js to start working on your app!</Text>
+        <Text> Hello World</Text>
+        <TouchableOpacity onPress={() => this.setState({ index: this.state.index + 1})}>
+          <Text>
+            Hello
+          </Text>
+        </TouchableOpacity>
+        <Text>
+          {this.state.index}
+        </Text>
       </View>
     );
   }
